4a99cda77f3407906e44e304207726574e3f7ef8
leevers <leevers@gmail.com>
2020-06-15 17:50:43 +1000

    fixes for linting

--- src/payphones.js
+++ src/payphones.js
@@ -24,15 +24,14 @@ function getPayphones(test, envelope) {
       let crs = null;
       let metadata = null;
 
-      let envelopeString = "112.91944420700005,-54.75042083099999,159.10645592500006,-9.240166924999869";
+      let envelopeString = '112.91944420700005,-54.75042083099999,159.10645592500006,-9.240166924999869';
 
       console.log(test);
       // console.log(envelope);
-      if(envelope)
-      {
+      if (envelope) {
         envelopeString = envelope;
       }
-      
+
       // this is used for timing the execution.
       console.time('Download complete');
 
